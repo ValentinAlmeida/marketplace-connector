27ed6106ad31cab39939c6c32282c7f923fe836f
ValentinAlmeida <valentinfalmeida@hotmail.com>
2025-05-21 12:21:03 -0300

    feat(import): added trait for errors

--- app/Listeners/Import/OnOffersDispatchedToHub.php
+++ app/Listeners/Import/OnOffersDispatchedToHub.php
@@ -5,38 +5,25 @@ namespace App\Listeners\Import;
 use App\Entities\States\Import\CompletedState;
 use App\Events\Import\OffersDispatchedToHub;
 use App\UseCase\Contracts\Import\IImportProcessor;
+use App\UseCase\Import\Support\HandlesImportFailures;
 use Illuminate\Support\Carbon;
 
 class OnOffersDispatchedToHub
 {
-    /**
-     * Create a new listener instance.
-     *
-     * @param ImportServiceInterface $importService Service to manage import entities.
-     */
+    use HandlesImportFailures;
+
     public function __construct(
         private IImportProcessor $importService
     ) {}
 
-    /**
-     * Handle the OffersSentToHub event.
-     *
-     * This listener marks the import as completed and sets the completion timestamp.
-     *
-     * @param OffersSentToHub $event The event signaling that offers were successfully sent to the hub.
-     * @return void
-     */
     public function handle(OffersDispatchedToHub $event): void
     {
-        $import = $this->importService->findImport($event->importId);
-        try {
+        $this->executeSafely($event->importId, function () use ($event) {
+            $import = $this->importService->findImport($event->importId);
             $import->changeState(new CompletedState());
             $import->setCompletedAt(Carbon::now());
 
             $this->importService->updateImport($import);
-        } catch (\Throwable $e) {
-            $import->fail($e->getMessage());
-            $this->importService->updateImport($import);
-        }
+        });
     }
 }
